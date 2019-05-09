4668543471f3302233fadad987509121915afe5a
Yipeng <reg20170621@163.com>
2019-05-09 08:55:29 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-#cd prepare_data
-#python prepare_data.py --mode train --img_dir ../data/bbx --label_file ../data/bbx/trainImageList.txt --img_size 48 --aug_scale 5
-#python prepare_data.py --mode val --img_dir ../data/bbx --label_file ../data/bbx/testImageList.txt --img_size 48 --aug_scale 1
-#cd ..
-
 cd prepare_data
-python prepare_data.py --mode train --img_dir ../data/human_bbx --label_file ../data/anime_bbx/anime_face_train.txt --img_size 48 --aug_scale 5
-python prepare_data.py --mode val --img_dir ../data/human_bbx --label_file ../data/anime_bbx/anime_face_val.txt --img_size 48 --aug_scale 1
+python prepare_data.py --mode train --img_dir ../data/bbx --label_file ../data/bbx/trainImageList.txt --img_size 48 --aug_scale 5
+python prepare_data.py --mode val --img_dir ../data/bbx --label_file ../data/bbx/testImageList.txt --img_size 48 --aug_scale 1
 cd ..
